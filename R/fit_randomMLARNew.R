76837b317a85876cb745da787f6d78ddf184a742
SimonStuber <simon.stuber1@gmail.com>
2023-01-26 17:56:12 +0100

    adapt interval

--- R/fit_randomMLARNew.R
+++ R/fit_randomMLARNew.R
@@ -403,7 +403,7 @@ U[1:3,1:3] <- uppertri_mult_diag(Ustar[1:3, 1:3], sds[1:3])
                         target=c("effMeans"),
                         control=list(tries=1,
                                      propCov=inits$U%*%t(inits$U),
-                                     adaptInterval=80,
+                                     adaptInterval=300,
                                      adaptFactorExponent=.8))
 
    mcmcConfig$removeSampler(c("eff"))
@@ -414,7 +414,7 @@ U[1:3,1:3] <- uppertri_mult_diag(Ustar[1:3, 1:3], sds[1:3])
                           target=c(eff[i]),
                           control=list(tries=2,
                                        propCov=diag(inits$eff[i,]),
-                                       adaptFactorExponent=.9,
+                                       adaptFactorExponent=.8,
                                        adaptInterval=500))
   }
 
@@ -431,7 +431,7 @@ U[1:3,1:3] <- uppertri_mult_diag(Ustar[1:3, 1:3], sds[1:3])
     mcmcConfig$addSampler(type = 'RW_block',
                           target=c("bb"),
                           control=list(tries=1,
-                                       adaptInterval=100,
+                                       adaptInterval=500,
                                        adaptFactorExponent=.8))
 
 
