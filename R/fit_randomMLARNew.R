65984a370c0730288ee20f414ab143981e5d014c
SimonStuber <simon.stuber1@gmail.com>
2023-01-26 21:45:28 +0100

    tune

--- R/fit_randomMLARNew.R
+++ R/fit_randomMLARNew.R
@@ -408,7 +408,7 @@ U[1:3,1:3] <- uppertri_mult_diag(Ustar[1:3, 1:3], sds[1:3])
 
    mcmcConfig$addSampler(type = 'crossLevel',
                          target=c("eff", "effMeans", "bb"),
-                         control=list("topNodes"=c("effMeans","Ustar"),
+                         control=list("topNodes"=c("effMeans"),
                                       "adaptFactorExponent"=.9,
                                       "adaptInterval"=500))
   # eff <- c()
