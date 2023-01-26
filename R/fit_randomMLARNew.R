aac2f63e1f8a33e68985f50e971ca3f4297a04c4
SimonStuber <simon.stuber1@gmail.com>
2023-01-27 00:10:59 +0100

    tune

--- R/fit_randomMLARNew.R
+++ R/fit_randomMLARNew.R
@@ -404,13 +404,12 @@ U[1:3,1:3] <- uppertri_mult_diag(Ustar[1:3, 1:3], sds[1:3])
    mcmcConfig$removeSampler(c("eff"))
    mcmcConfig$removeSampler(c("Ustar"))
 
-   mcmcConfig$addSampler(type="slice", target="effMeans[3]")
+   mcmcConfig$addSampler(type="RW", target="effMeans[3]")
 
 
    mcmcConfig$addSampler(type = 'RW_block_lkj_corr_cholesky',
                          target=c("Ustar"),
-                         control=list("topNodes"=c("effMeans"),
-                                      "adaptFactorExponent"=.95,
+                         control=list("adaptFactorExponent"=.95,
                                       "adaptInterval"=50,
                                       "scale"=0.2))
 
