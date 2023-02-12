90396ec9975942ed5bde055589a6c6282f205306
SimonStuber <simon.stuber1@gmail.com>
2023-02-12 21:39:46 +0100

    f .9, 500 t .8 100

--- R/fit_randomMLARNew_GAMMA.R
+++ R/fit_randomMLARNew_GAMMA.R
@@ -520,8 +520,8 @@ fit_randomMLAR_G <- function(y, niter=30000, nburnin=20000,
                         target=("res.var"),
                         control=list(reflective=FALSE,
                                      scale=inits$res.var/2,
-                                     adaptFactorExponent=.9,
-                                     adaptInterval=500,
+                                     adaptFactorExponent=.8,
+                                     adaptInterval=200,
                                      tries=2))
   mcmcConfig$removeSampler(c("Ustar"))
   mcmcConfig$addSampler(type = "RW_block_lkj_corr_cholesky",
